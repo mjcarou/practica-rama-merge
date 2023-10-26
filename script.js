d35bf2067008424596a8ef9f0116d5c792adac8f
mjcarou <mjcarou@hotmail.com>
2023-10-26 13:32:33 -0300

    Quite el espacio

--- script.js
+++ script.js
@@ -14,6 +14,5 @@ function agregarALaPantalla(value) {
   
   function deshacer() {
     const valorPantalla = document.getElementById('pantalla').value;
-
     document.getElementById('pantalla').value = valorPantalla.slice(0,-1);
   }
